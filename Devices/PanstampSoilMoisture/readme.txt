08fea5d03d6f8b84c1d3b5e2ae9193f03468dc54
Huy Do <huy.do@gmx.de>
2014-04-30 10:35:08 +0200

    added panstamp soilmoisture device

---
+++ Devices/PanstampSoilMoisture/readme.txt
@@ -0,0 +1,5 @@
+based on http://www.panstamp.com/downloads
+
+panstamp_sketches.zip, 209k	v. 28	May 18, 2013, 9:45 PM	Daniel Berenguer
+
+renamed *.ino *.cpp
---
+++ Devices/PanstampSoilMoisture/src/board.h
@@ -0,0 +1,12 @@
+#ifndef __BOARD_H
+#define __BOARD_H
+
+/**
+ * Sensor pins
+ */
+#define SENSOR_0_PIN  3     // Analog pin - sensor 0
+#define POWER_0_PIN   16    // Digital pin used to powwer sensor 0
+#define SENSOR_1_PIN  5     // Analog pin - sensor 1
+#define POWER_1_PIN   18    // Digital pin used to powwer sensor 1
+
+#endif /* end __BOARD_H */
