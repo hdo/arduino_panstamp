58aef61b3fe729ab97a8680ef13b10ea8050ed8d
Huy Do <huy.do@gmx.de>
2014-05-01 20:39:34 +0200

    added capacity based soil moisture sensor 2

---
+++ Devices/PanstampSoilMoisture2/src/board.h
@@ -0,0 +1,10 @@
+#ifndef __BOARD_H
+#define __BOARD_H
+
+/**
+ * Sensor pins
+ */
+#define SENSOR_0_PIN  5     // Analog pin - sensor 0
+#define POWER_0_PIN   6    // Digital pin used to powwer sensor 0
+
+#endif /* end __BOARD_H */
