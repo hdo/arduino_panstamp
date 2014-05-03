ace08c979f15b55e8776c6f71325520d59f3bc62
Huy Do <huy.do@gmx.de>
2014-05-03 10:27:11 +0200

    added panstamp temperature sensor based on DS18B20

---
+++ Devices/PanstampTemperature/src/board.h
@@ -0,0 +1,10 @@
+#ifndef __BOARD_H
+#define __BOARD_H
+
+/**
+ * Sensor pins
+ */
+#define SENSOR_0_PIN  2     // Digital pin - sensor 0
+#define POWER_0_PIN   7    // Digital pin used to powwer sensor 0
+
+#endif /* end __BOARD_H */
